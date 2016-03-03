fcca5f10106cb614218b1788743b4b9be117f5ba
Mohamad Taufiq <tfqnet@gmail.com>
2016-03-03 18:07:05 +0800

    init app

---
+++ demoApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  demoApp
+//
+//  Created by PETRONAS ICT SDN BHD on 3/3/16.
+//  Copyright © 2016 myCompany. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ demoApp/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  demoApp
+//
+//  Created by PETRONAS ICT SDN BHD on 3/3/16.
+//  Copyright © 2016 myCompany. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
